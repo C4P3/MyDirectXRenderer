6890886b34a5d902488485b5156529ab14a92dd2
augmented11th <misaqui0408@gmail.com>
2026-09-03 10:53:31 +0900

    a

---
+++ MyDirectXRenderer/RenderGraph/RenderGraph.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace rg {
+
+	constexpr uint16_t kInvalidIndex = 0xffff;
+	constexpr uint16_t kNoPass = 0xffff;
+
+	struct TextureHandle {
+
+	};
+}
+
